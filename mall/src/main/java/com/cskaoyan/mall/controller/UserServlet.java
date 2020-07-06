83daba83334ad1aaa5cd464febc4c6b054fba227
Alex-Jee <jxfmail@foxmail.com>
2020-07-06 23:36:13 +0800

    5

---
+++ mall/src/main/java/com/cskaoyan/mall/controller/UserServlet.java
@@ -0,0 +1,17 @@
+package com.cskaoyan.mall.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/admin/user/*")
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
